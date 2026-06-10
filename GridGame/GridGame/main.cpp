b714fbb71a2efc686e0abff32aded3dfab6dbc64
Heather Braden-Wilson <heatherbradenwilson@gmail.com>
2026-06-10 16:52:40 -0600

    initial commit

---
+++ GridGame/GridGame/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "raylib.h"
+#include <string>
+#include <vector>
+
+int main()
+{
+	InitWindow(800, 600, "Grid Lock");
+
+	while (!WindowShouldClose())
+	{
+
+
+	}
+	CloseWindow();
+
+	return 0;
+	
+}
