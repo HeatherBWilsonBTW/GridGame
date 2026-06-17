cb9680e1a3e12a796054a6e3f166a7168755a535
Heather Braden-Wilson <heatherbradenwilson@gmail.com>
2026-06-17 09:56:24 -0600

    Add player movement and ship rendering

---
+++ GridGame/GridGame/Salvage.cpp
@@ -0,0 +1,1 @@
+#include "Salvage.h"
--- GridGame/GridGame/main.cpp
+++ GridGame/GridGame/main.cpp
@@ -1,17 +1,57 @@
 #include <iostream>
 #include "raylib.h"
 #include <string>
-#include <vector>
+
 
 int main()
 {
-	InitWindow(800, 600, "Grid Lock");
+	std::cout << "Before InitWindow\n";
+	// create Window
+	InitWindow(800, 600, "Space Hunter");
+	std::cout << "After InitWindow\n";
+	// Set FPS
+	SetTargetFPS(60);
 
+	int playerX = 400;
+	int playerY = 300;
+	
 	while (!WindowShouldClose())
 	{
+		// input movement
+		// If w is pressed
+		if (IsKeyDown(KEY_W))
+		{
+			playerY -= 5;
+		}
+		//if s is pressed
+		if (IsKeyDown(KEY_S)) 
+		{
+			playerY += 5;
+		}
 
+		//if a is pressed 
+		if (IsKeyDown(KEY_A))
+		{
+			playerX -= 5;
+		}
 
+		//if d is preessed
+		if (IsKeyDown(KEY_D))
+		{
+			playerX += 5;
+		}
+
+		BeginDrawing();
+		ClearBackground(BLACK);
+		DrawTriangle(
+			{ (float)playerX, (float)playerY - 20 },
+			{ (float)playerX - 20, (float)playerY + 20 },
+			{ (float)playerX + 20, (float)playerY + 20 },
+			BLUE
+		);
+		EndDrawing();
 	}
+	// close window
 	CloseWindow();
 
 	return 0;
